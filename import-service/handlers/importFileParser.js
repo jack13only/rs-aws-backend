ba1b5f011620124f9532f385ef394f2d8f875daf
Jack <jack13only@gmail.com>
2022-10-09 16:42:13 +0300

    feat: add move files to parsed

--- import-service/handlers/importFileParser.js
+++ import-service/handlers/importFileParser.js
@@ -4,9 +4,9 @@ import { headers } from '../helpers/headers.js'
 import AWS from 'aws-sdk';
 import csv from 'csv-parser'
 
-export const importFileParser = async (event) => {
+export const importFileParser = async () => {
   console.log(`importFileParser`)
-  let file;
+  let files;
   const results = [];
   try {
     const { REGION, BUCKET } = process.env
@@ -19,22 +19,43 @@ export const importFileParser = async (event) => {
       Delimiter: '/',
     }
 
-    file = await s3.listObjectsV2(paramsList).promise()
+    files = await s3.listObjectsV2(paramsList).promise()
 
-    if (file.Contents.length) {
-      const paramsGet = {
-        Bucket: BUCKET,
-        Key: file.Contents[0].Key,
+    if (files.Contents.length) {
+      const readFile = (file) => new Promise((resolve, reject) => {
+        const paramsGet = {
+          Bucket: BUCKET,
+          Key: file.Key,
+        }
+
+        s3
+          .getObject(paramsGet)
+          .createReadStream()
+          .pipe(csv())
+          .on('data', (data) => results.push(data))
+          .on('end', async () => {
+            await s3.copyObject({
+              Bucket: BUCKET,
+              CopySource: BUCKET + '/' + file.Key,
+              Key: file.Key.replace('uploaded', 'parsed')
+            }).promise()
+
+            await s3.deleteObject({
+              Bucket: BUCKET,
+              Key: file.Key,
+            }).promise()
+            resolve()
+          });
+        })
+      
+      const readFiles = async (files) => {
+        for (let file of files) {
+          await readFile(file);
+        }
       }
   
-      const s3stream = s3
-        .getObject(paramsGet)
-        .createReadStream()
-        .pipe(csv())
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-          console.log(results);
-        });
+      await readFiles(files.Contents);
+      console.log(results)
     }
   } catch (err) {
     return {
