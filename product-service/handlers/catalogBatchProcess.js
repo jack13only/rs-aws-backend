e7260f2c315f2979c1cbf781c8a08ef5e297b527
Jack <jack13only@gmail.com>
2022-10-16 18:07:10 +0300

    feat: add catalogBatchProcess unit tests

--- product-service/handlers/catalogBatchProcess.js
+++ product-service/handlers/catalogBatchProcess.js
@@ -5,12 +5,14 @@ import { headers } from '../helpers/headers.js'
 import { validateData, createNewProduct } from '../helpers/functions.js'
 
 export const catalogBatchProcess = async (event) => {
+  let count = 0
+  let rim = 'no'
+
   try {
     const products = event.Records.map(({ body }) => JSON.parse(body))
     const dynamo = new AWS.DynamoDB.DocumentClient()
     const sns = new AWS.SNS({ region: process.env.REGION })
-    console.log('Products', products)
-
+    
     for (const product of products){
       if (validateData(product)) {        
         const {newProductForSale, newProductStock} = createNewProduct(product)
@@ -24,23 +26,34 @@ export const catalogBatchProcess = async (event) => {
           TableName: process.env.STOCKS_TABLENAME,
           Item: newProductStock
         }).promise()
+
+        count++
+        if (['rick', 'morty', 'summer'].includes(product.title.toLowerCase())) {
+          rim = 'yes'
+        }
       }
     }
 
     await sns.publish({
-      Subject: 'Products created!',
+      Subject: `${count} products created!`,
       Message: JSON.stringify(products),
       TopicArn: process.env.SNS_ARN,
       MessageAttributes: {
         rim: {
           DataType: 'String',
-          StringValue: products.find((product) => ['rick', 'morty', 'summer'].includes(product.title.toLowerCase())) ? 'yes' : 'no'
+          StringValue: rim
         }
     },
     }).promise()
 
+    if (!count) return {
+      statusCode: 404,
+      headers,
+      body: JSON.stringify(`No valid products in this batch!`),
+    };
+
   } catch(err) {
-    console.log(err)
+
     return {
       statusCode: 500,
       headers,
@@ -53,6 +66,6 @@ export const catalogBatchProcess = async (event) => {
   return {
     statusCode: 200,
     headers,
-    body: JSON.stringify('catalogBatchProcess done!'),
+    body: JSON.stringify(`${count} products created!`),
   };
 };
---
+++ product-service/handlers/catalogBatchProcess.test.js
@@ -0,0 +1,119 @@
+import mockAWS from 'aws-sdk-mock'
+import { catalogBatchProcess } from './catalogBatchProcess'
+
+// const event = {
+//   Records: [
+//     {
+//       body: '{"title":"Toy4","description":"Toy4","price":"43","count":"11"}',
+//     }
+//   ]
+// }
+
+describe('success catalogBatchProcess lambda', () => {
+  it('1 product successfully created (from 1 valid)', async () => {
+    mockAWS.mock('DynamoDB.DocumentClient', 'put', '')
+    mockAWS.mock('SNS', 'publish', '')
+
+    const event = {
+      Records: [
+        {
+          body: '{"title":"Toy1","description":"Toy1","price":"1","count":"11"}',
+        }
+      ]
+    }
+
+    const responce = await catalogBatchProcess(event)
+
+    expect(responce.body.includes('1 products created!')).toBe(true)
+    expect(responce.statusCode).toBe(200)
+  })
+
+  it('3 product successfully created (from 3 valid)', async () => {
+    mockAWS.mock('DynamoDB.DocumentClient', 'put', '')
+    mockAWS.mock('SNS', 'publish', '')
+
+    const event = {
+      Records: [
+        {
+          body: '{"title":"Toy1","description":"Toy1","price":"11","count":"111"}',
+        },
+        {
+          body: '{"title":"Toy2","description":"Toy2","price":"22","count":"222"}',
+        },
+        {
+          body: '{"title":"Toy3","description":"Toy3","price":"33","count":"333"}',
+        },
+      ]
+    }
+
+    const responce = await catalogBatchProcess(event)
+
+    expect(responce.body.includes('3 products created!')).toBe(true)
+    expect(responce.statusCode).toBe(200)
+  })
+
+  it('1 product successfully created (from 1 valid and 2 invalid) ', async () => {
+    mockAWS.mock('DynamoDB.DocumentClient', 'put', '')
+    mockAWS.mock('SNS', 'publish', '')
+
+    const event = {
+      Records: [
+        {
+          body: '{"title":"Toy1","description":"Toy1","price":"11","count":"111"}',
+        },
+        {
+          body: '{"description":"Toy2","price":"22","count":"222"}',
+        },
+        {
+          body: '{"title":"Toy3"}',
+        },
+      ]
+    }
+
+    const responce = await catalogBatchProcess(event)
+
+    expect(responce.body.includes('1 products created!')).toBe(true)
+    expect(responce.statusCode).toBe(200)
+  })
+
+  it('0 product successfully created (from 2 invalid) ', async () => {
+    mockAWS.mock('DynamoDB.DocumentClient', 'put', '')
+    mockAWS.mock('SNS', 'publish', '')
+
+    const event = {
+      Records: [
+        {
+          body: '{"description":"Toy2","price":"22","count":"222"}',
+        },
+        {
+          body: '{"title":"Toy3"}',
+        },
+      ]
+    }
+
+    const responce = await catalogBatchProcess(event)
+
+    expect(responce.body.includes('No valid products in this batch!')).toBe(true)
+    expect(responce.statusCode).toBe(404)
+  })
+})
+
+describe('unsuccess catalogBatchProcess lambda', () => {
+  it('wrong event', async () => {
+    mockAWS.mock('DynamoDB.DocumentClient', 'put', '')
+    mockAWS.mock('SNS', 'publish', '')
+
+    const event = {
+      Records: [
+        {
+          message: '{"title":"Toy3"}',
+        },
+      ]
+    }
+
+    const responce = await catalogBatchProcess(event)
+  
+    expect(responce.body.includes('Unexpected token u in JSON at position 0')).toBe(true)
+    expect(responce.statusCode).toBe(500)
+  })
+})
