ba934832e3bce081ae239b7d4dc912caaa84a0d8
Jack <jack13only@gmail.com>
2022-10-21 00:00:38 +0300

    feat: add authorizer

--- authorization-service/handlers/basicAuthorizer.js
+++ authorization-service/handlers/basicAuthorizer.js
@@ -3,23 +3,60 @@ dotenv.config()
 import { headers } from '../helpers/headers.js'
 import AWS from 'aws-sdk';
 
-export const basicAuthorizer = async () => {
+export const basicAuthorizer = async (event) => {
   console.log(`--- basicAuthorizer started ---`)
-  
-  try {
-  } catch (err) {
-    return {
-      statusCode: 500,
-      headers,
-      body: JSON.stringify({
-        message: err.message
-      }),
-    }
-  }
+  console.log(`event: `, event)
 
-  return {
-    statusCode: 200,
-    headers,
-    body: JSON.stringify('ok'),
-  };
+  // try {
+  //   const encodedCreds = event.autorizationToken.split(' ')[1]
+  //   const buff = Buffer.from(encodedCreds, 'base64')
+
+  //   const creds = buff.toString('utf-8').split(':')
+  //   const [name, pass] = creds
+
+  //   console.log(`name: `, name, `pass: `, pass)
+
+    return {
+      principalId: "yyyyyyyy",
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: event.methodArn
+          }
+        ]
+      },
+    }
+
+    // return {
+    //   principalId: 'encodedCreds',
+    //   policyDocument: {
+    //     Version: '2012-10-17',
+    //     Statement: [
+    //       {
+    //         Action: 'execute-api:Invoke',
+    //         Effect: 'Allow',
+    //         Resource: event.methodArn
+    //       }
+    //     ]
+    //   }
+    // }
+
+  // } catch (err) {
+  //   return {
+  //     principalId: 'encodedCreds',
+  //     policyDocument: {
+  //       Version: '2012-10-17',
+  //       Statement: [
+  //         {
+  //           Action: 'execute-api:Invoke',
+  //           Effect: 'Allow',
+  //           Resource: 'event.methodArn'
+  //         }
+  //       ]
+  //     }
+  //   }
+  // }
 };
