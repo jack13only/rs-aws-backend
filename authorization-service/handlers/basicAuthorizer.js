155cef4c27bca402318f55828f221dba3636a526
Jack <jack13only@gmail.com>
2022-10-23 14:23:32 +0300

    feat: add authorizer arn to env

--- authorization-service/handlers/basicAuthorizer.js
+++ authorization-service/handlers/basicAuthorizer.js
@@ -4,59 +4,57 @@ import { headers } from '../helpers/headers.js'
 import AWS from 'aws-sdk';
 
 export const basicAuthorizer = async (event) => {
-  console.log(`--- basicAuthorizer started ---`)
   console.log(`event: `, event)
 
-  // try {
-  //   const encodedCreds = event.autorizationToken.split(' ')[1]
-  //   const buff = Buffer.from(encodedCreds, 'base64')
-
-  //   const creds = buff.toString('utf-8').split(':')
-  //   const [name, pass] = creds
-
-  //   console.log(`name: `, name, `pass: `, pass)
-
-    return {
-      principalId: "yyyyyyyy",
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Allow',
-            Resource: event.methodArn
-          }
-        ]
-      },
+  try {
+    const { authorizationToken } = event
+    if (!authorizationToken) {
+      return {
+        statusCode: 401,
+        headers,
+        body: JSON.stringify({
+          message: err.message
+        }),
+      }
     }
+    const encodedCreds = authorizationToken.split(' ')[1]
+    const buff = Buffer.from(encodedCreds, 'base64')
 
-    // return {
-    //   principalId: 'encodedCreds',
-    //   policyDocument: {
-    //     Version: '2012-10-17',
-    //     Statement: [
-    //       {
-    //         Action: 'execute-api:Invoke',
-    //         Effect: 'Allow',
-    //         Resource: event.methodArn
-    //       }
-    //     ]
-    //   }
-    // }
+    const creds = buff.toString('utf-8').split(':')
+    const [name, pass] = creds
 
-  // } catch (err) {
-  //   return {
-  //     principalId: 'encodedCreds',
-  //     policyDocument: {
-  //       Version: '2012-10-17',
-  //       Statement: [
-  //         {
-  //           Action: 'execute-api:Invoke',
-  //           Effect: 'Allow',
-  //           Resource: 'event.methodArn'
-  //         }
-  //       ]
-  //     }
-  //   }
-  // }
+    console.log(`name: `, name, `pass: `, pass)
+
+    if (process.env[name] === pass) {
+      return {
+        principalId: name,
+        policyDocument: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Action: 'execute-api:Invoke',
+              Effect: 'Allow',
+              Resource: event.methodArn
+            }
+          ]
+        },
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  return {
+    principalId: 'Anonimus',
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: 'Deny',
+          Resource: event.methodArn
+        }
+      ]
+    },
+  }
 };
