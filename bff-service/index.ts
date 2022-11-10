34081bc3ffee07abfbddb11daf12a83d753b6f02
Jack <jack13only@gmail.com>
2022-11-11 00:32:46 +0300

    feat: add import endpoint

--- bff-service/index.ts
+++ bff-service/index.ts
@@ -17,9 +17,9 @@ app.use((req, res, next) => {
   const data = `${req.method} ${req.url}`;
   console.log(data);  
   next();
-});
+})
 
-app.get('/products', async (req, res, next) => {
+app.get('/products', async (req, res) => {
   try {
     if (!process.env.products) res.status(502).send('Cannot process request')
     const responce = await fetch(process.env.products || '')
@@ -32,7 +32,7 @@ app.get('/products', async (req, res, next) => {
   }
 })
 
-app.get('/products/:id', async (req, res, next) => {
+app.get('/products/:id', async (req, res) => {
   try {
     if (!process.env.products) res.status(502).send('Cannot process request')
     const responce = await fetch(`${process.env.products || ''}/${req.params.id}`)
@@ -45,18 +45,37 @@ app.get('/products/:id', async (req, res, next) => {
   }
 });
 
-app.get('/cart', async (req, res, next) => {
+app.get('/cart', async (req, res) => {
   try {
     if (!process.env.cart) res.status(502).send('Cannot process request')
-    const aaa = await fetch(process.env.cart || '')
-    const data = await aaa.json()
-    res.status(aaa.status).send(data)
+    const responce = await fetch(process.env.cart || '')
+    const data = await responce.json()
+    res.status(responce.status).send(data)
   } catch (err: unknown) {
     if (err instanceof Error) {
       res.status(502).send(err.message);
     }
   }
-});
+})
+
+app.get('/import', async (req, res) => {
+  console.log(JSON.stringify({headers: {'authorization': req.headers['authorization'] || ''}}))
+  console.log(`${process.env.import || ''}?name=${req.query['name']}`)
+  try {
+    if (!req.query['name']) res.status(502).send('No filename provided')
+    if (!process.env.import) res.status(502).send('Cannot process request')
+    const responce = await fetch(`${process.env.import || ''}?name=${req.query['name']}`, {
+      method: 'get',
+      headers: {'authorization': req.headers['authorization'] || ''}
+    })
+    const data = await responce.json()
+    res.status(responce.status).send(data)
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(502).send(err.message);
+    }
+  }
+})
 
 app.get('*', (req, res) => {
   res.status(502).send('Cannot process request')
