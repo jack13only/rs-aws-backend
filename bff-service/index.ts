5b94e735778e419289790ef8cd380348bba91326
Jack <jack13only@gmail.com>
2022-11-10 23:47:24 +0300

    feat: add products endpoint

--- bff-service/index.ts
+++ bff-service/index.ts
@@ -1,10 +1,8 @@
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-unresolved */
-/* eslint-disable no-console */
 import express from 'express';
 import 'dotenv/config';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import fetch from 'node-fetch';
 
 const app = express();
 const port = process.env.PORT || '3000';
@@ -21,9 +19,34 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/products', async (req, res, next) => {
+  try {
+    if (!process.env.products) res.status(502).send('Cannot process request')
+    const responce = await fetch(process.env.products || '')
+    const data = await responce.json()
+    res.status(responce.status).send(data)
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(502).send(err.message);
+    }
+  }
+});
+
+app.get('/products/:id', async (req, res, next) => {
+  try {
+    if (!process.env.products) res.status(502).send('Cannot process request')
+    const responce = await fetch(`${process.env.products || ''}/${req.params.id}`)
+    const data = await responce.json();
+    res.status(responce.status).send(data)
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(502).send(err.message);
+    }
+  }
+});
+
 app.get('*', (req, res) => {
-  res.status(404);
-  res.send('Service not found!');
+  res.status(502).send('Cannot process request')
 });
 
 app.listen(port, () => console.log(`Server is listening on port ${port}!`));
